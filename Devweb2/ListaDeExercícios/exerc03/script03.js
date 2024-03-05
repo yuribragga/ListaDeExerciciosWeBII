7a9412eda83928f54cc939c2c860e82fea4055e2
yuribragga <79583088+yuribragga@users.noreply.github.com>
2024-03-05 08:45:34 -0300

    Atualização dos exercícios 03 e 06

--- Devweb2/ListaDeExercícios/exerc03/script03.js
+++ Devweb2/ListaDeExercícios/exerc03/script03.js
@@ -1,67 +1,84 @@
+const rastreamento = document.getElementsByName('radioRastreiamento')
+const regiao = document.getElementsByName('radioRegiao')
+const quantidadePecas = document.querySelector('#pecasNumber')
+const distaciaKm = document.querySelector('#pecasKm')
+const resp = document.querySelector('#resultado')
 
-const rastreamento = document.getElementsByName('radioRastreiamento');
-const regiao = document.getElementsByName('radioRegiao');
-const quantidadePecas = document.querySelector('#pecasNumber');
-const distanciaKm = document.querySelector('#pecasKm');
-const resp = document.querySelector('#resultado');
-
-const combustivel = 5.00;
-let regiaoEscolhida;
-let valorFinal = 0;
-let fretePecas = 0;
-let freteKm = 0;
+const combustivel = 5.00
+let regiaoEscolhida
+let valorFinal = 0
+let fretePecas = 0
+let freteKm = 0
 
 const frete = {
     Sul: 1.0,
-    Sudeste: 1.20,
-    CO: 0.13
-};
+    Suldeste: 1.20,
+    CO: 1.30
+}
 
-resp.innerHTML = '';
+const freteDesconto = {
+    Sul: 0.10,
+    Suldeste: 0.12,
+    CO: 0.13
+}
+
+resp.innerHTML = ''
 
 function determinaTaxaRastreio() {
-    if (rastreamento[0].checked){
-        valorFinal += 200;
-        return 'R$200,00';
+    if (rastreamento[0].checked) {
+        valorFinal += 200
+        return 'R$200,00'
     }
-    else {
-        valorFinal += 0;
-        return 'O rastreio não foi escolhido!';
+     else {
+        valorFinal += 0
+        return 'O rastreio não foi escolhido!'
     }
 }
 
 function determinaRegiao(){
-    if (regiao[0].checked){
-        return 'Sul';
+    if (regiao[0].checked) {
+        return 'Sul'
+
+    } 
+    else if (regiao[1].checked) {
+        return 'Suldeste'
+
     }
-    else if (regiao[1].checked){
-        return 'Sudeste';
-    }
-    else {
-        return 'CO';
+     else {
+        return 'CO'
     }
 }
 
 function calcula() {
-    const quantidade = Number(quantidadePecas.value);
-    const distancia = Number(distanciaKm.value);
-    const taxaRastreio = determinaTaxaRastreio();
-    const regiaoEscolhida = determinaRegiao();
 
+    const quantidade = Number(quantidadePecas.value)
+    const distacia = Number(distaciaKm.value)
+    const taxaRastreio = determinaTaxaRastreio()
+    const regiaoEscolhida = determinaRegiao()
+    
     if (quantidade < 1000) {
-        fretePecas = (frete[regiaoEscolhida] * quantidade).toFixed(2);
-        freteKm = (combustivel * distancia).toFixed(2);
-        valorFinal += (frete[regiaoEscolhida] * quantidade) * (combustivel * distancia);
-    } else {
-        let diferenca = quantidade - 1000;
-        let valorComDesconto = frete[regiaoEscolhida] - (frete[regiaoEscolhida] * freteDesconto[regiaoEscolhida]);
-        fretePecas = ((frete[regiaoEscolhida] * 1000) + (diferenca * valorComDesconto)).toFixed(2);
-        freteKm = (combustivel * distancia).toFixed(2);
-        valorFinal += (frete[regiaoEscolhida] * 1000) + (diferenca * valorComDesconto) + (combustivel * distancia);
+
+        fretePecas = ((frete[regiaoEscolhida] * quantidade)).toFixed(2)
+
+        freteKm = (combustivel * distacia).toFixed(2)
+
+        valorFinal += (frete[regiaoEscolhida] * quantidade) + (combustivel * distacia)
+
+    } 
+    else {
+        let diferença = quantidade - 1000
+
+        let valorComDesconto = frete[regiaoEscolhida] - (frete[regiaoEscolhida] * freteDesconto[regiaoEscolhida])
+
+        fretePecas = ((frete[regiaoEscolhida] * 1000) + (diferença * valorComDesconto)).toFixed(2)
+
+        freteKm = (combustivel * distacia).toFixed(2)
+
+        valorFinal += (frete[regiaoEscolhida] * 1000) + (diferença * valorComDesconto) + (combustivel * distacia)
     }
 
     resp.innerHTML = `<p>Taxa de rastreamento: ${taxaRastreio}</p>
-                    <p>Valor do frete pelas peças: R$${fretePecas}</p>
-                    <p>Valor do frete por quilômetro: R$${freteKm}</p>
-                    <p>Valor do frete: R$${valorFinal.toFixed(2)}</p>`;
+                      <p>Valor do frete pelas peças: R$${fretePecas}</p>
+                      <p>Valor do frete por quilometro: R$${freteKm}</p>
+                      <p>Total do frete: R$${valorFinal.toFixed(2)}</p>`
 }
--- Devweb2/ListaDeExercícios/exerc06/script06.js
+++ Devweb2/ListaDeExercícios/exerc06/script06.js
@@ -1,41 +1,47 @@
-const data = prompt('Digite uma data: (Ex: 27/02/2024) ').split('/')
-let mesDescricao
-switch(data[1]){
-    case '01':
-        mesDescricao = 'janeiro'
-        break
-    case '02':
-        mesDescricao ='fevereiro'
-        break
-    case '03':
-        mesDescricao ='março'
-        break
-    case '04':
-        mesDescricao ='abril'
-        break
-    case '05':
-        mesDescricao ='maio'
-        break
-    case '06':
-        mesDescricao ='junho'
-        break
-    case '07':
-        mesDescricao = 'julho'
-        break
-    case '08':
-        mesDescricao = 'agosto'
-        break
-    case '09':
-        mesDescricao = 'setembro'
-        break
-    case '10':
-        mesDescricao ='outubro'
-        break
-    case '11':
-        mesDescricao ='novembro'
-        break
-    default:
-        mesDescricao = 'dezembro'
-        break
+function criardata(){
+    var datastring = document.getElementById("data").value;
+    let partes = datastring.split("/")
+    let mesDescricao
+    switch(partes[1]){
+        case '01':
+            mesDescricao = 'janeiro'
+            break
+        case '02':
+            mesDescricao ='fevereiro'
+            break
+        case '03':
+            mesDescricao ='março'
+            break
+        case '04':
+            mesDescricao ='abril'
+            break
+        case '05':
+            mesDescricao ='maio'
+            break
+        case '06':
+            mesDescricao ='junho'
+            break
+        case '07':
+            mesDescricao = 'julho'
+            break
+        case '08':
+            mesDescricao = 'agosto'
+            break
+        case '09':
+            mesDescricao = 'setembro'
+            break
+        case '10':
+            mesDescricao ='outubro'
+            break
+        case '11':
+            mesDescricao ='novembro'
+            break
+        case '12':
+            mesDescricao = 'dezembro'
+            break
+        default:
+            document.getElementById('resultado').innerText = "Inválido";
+            return;
+    }
+    document.getElementById('resultado').innerText = partes[0] + " de " + mesDescricao + " de " + partes[2];
 }
-alert(`${data[0]} de ${mesDescricao} de ${data[2]}`)
